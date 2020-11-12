ee59a4435be88c80f73e466b20a803fd7ea5f7f3
Jack <jackmleitch@gmail.com>
2020-11-12 16:37:28 -0700

    entity_embedding

--- Categorical variables/cat_in_dat/src/entity_embedding.py
+++ Categorical variables/cat_in_dat/src/entity_embedding.py
@@ -11,4 +11,109 @@ from tensorflow.keras import backend as K
 def create_model(data, catcols):
     """
     This function returns a compiled tf.keras model
+    :param data: pandas dataframe
+    :param catcols: list of categorical column names in dataframe
+    :return: compiled tf.keras model 
     """
+
+    # init list of inputs for embeddings
+    inputs = []
+
+    # init list of outputs for embeddings
+    outputs = []
+
+    # loop over all cat columns
+    for c in catcols:
+        # find number of unique values in column 
+        num_unique_values = int(data[c].nunique())
+        # we next calculate dimension of embedding
+        # min size is half # of unique values
+        # max size is 50
+        embed_dim = int(min(np.ceil(num_unique_values/2),50))
+
+        # simple keras input layer with size 1
+        inp = layers.Input(shape=(1,))
+
+        # add embedding layer to raw input 
+        # embedding size is always 1 more than unique values in input
+        out = layers.Embedding(
+            num_unique_values + 1, embed_dim, name=c
+            )(inp)
+
+        # 1-d spacial dropout is the standard for embedding layers 
+        out = layers.SpatialDropout1D(0.3)(out)
+
+        # reshape input to dimension of embedding
+        # this becomes our output layer for current feature
+        out = layers.Reshape(target_shape=(embed_dim, ))(out)
+
+        # add input to input list
+        inputs.append(inp)
+
+        # add output to output list
+        outputs.append(out)
+
+    # concatenate all output layers
+    x = layers.Concatenate()(outputs)
+
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Dense(300, activation='relu')(x)
+    x = layers.Dropout(0.3)(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Dense(300, activation='relu')(x)
+    x = layers.Dropout(0.3)(x)
+    x = layers.BatchNormalization()(x)
+
+    y = layers.Dense(2, activation='softmax')(x)
+
+    # create final model 
+    model = Model(inputs=inputs, outputs=y)
+
+    # compile the model 
+    model.compile(loss='binary_crossentropy', optimizer='adam')
+    return model 
+
+def run(fold):
+
+    df = pd.read_csv('../input/train_fold.csv')
+    features = [f for f in df.columns if f not in ('id', 'target', 'kfold')]
+
+    for col in features:
+        df.loc[:, col] = df[col].astype(str).fillna("NONE")
+
+    for feat in features:
+        lbl_enc = preprocessing.LabelEncoder()
+        df.loc[:, feat] = lbl_enc.fit_transform(df[feat].values)
+
+    df_train = df[df.kfold != fold].reset_index(drop=True)
+    df_valid = df[df.kfold == fold].reset_index(drop=True)
+
+    model = create_model(df, features)
+
+    # our features are lists of lists
+    xtrain = [
+        df_train[features].values[:, k] for k in range(len(features))
+    ]
+    xvalid = [
+        df_valid[features].values[:, k] for k in range(len(features))
+    ]
+
+    ytrain = df_train.target.values
+    yvalid = df_valid.target.values
+
+    ytrain_cat = utils.to_categorical(ytrain)
+    yvalid_cat = utils.to_categorical(yvalid)
+
+    model.fit(xtrain, ytrain_cat, validation_data=(xvalid, yvalid_cat), verbose=1, batch_size=1024, epochs=3)
+
+    valid_preds = model.predict(xvalid)[:, 1]
+
+    print(metrics.roc_auc_score(yvalid, valid_preds))
+
+    K.clear_session()
+
+if __name__ == "__main__":
+    for i in range(5):
+        run(i)
